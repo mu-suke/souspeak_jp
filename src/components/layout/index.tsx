c800e1687258ac1d8f96ed80c9419b94104d5f00
mu-suke <chelsea_08ym@yahoo.co.jp>
2023-08-14 21:22:02 +0900

    feat: 工事中ページの作成

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,32 @@
+import { Box, Container } from '@chakra-ui/react'
+import React from 'react'
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+
+const Layout: React.FC<{
+  containerVariant?: 'xs' | 'sm' | 'lg' | 'xl' | 'default'
+  isWhiteBg?: boolean
+  children: React.ReactNode
+}> = ({ containerVariant = 'default', isWhiteBg = false, children }) => {
+  return (
+    <Box
+      backgroundColor={'bg'}
+      // Footerを下部に固定
+      minHeight="100vh"
+      position="relative"
+      // FooterHeight
+      paddingBottom="5rem"
+    >
+      <Header />
+      <Container
+        backgroundColor={isWhiteBg ? 'white' : ''}
+        variant={containerVariant}
+      >
+        {children}
+      </Container>
+      <Footer />
+    </Box>
+  )
+}
+
+export default Layout
---
+++ src/components/theme/components/container.ts
@@ -0,0 +1,55 @@
+export const Container = {
+  variants: {
+    default: {
+      maxW: 'container.md',
+      mx: 'auto',
+      paddingX: { base: 0, md: '1.5rem' },
+      paddingY: {
+        base: 0,
+        md: '1.5rem',
+      },
+    },
+    xl: {
+      maxW: 'container.xl',
+      mx: 'auto',
+      // paddingX: { base: 0, md: '1.5rem' },
+      paddingY: {
+        base: 0,
+        md: '1.5rem',
+      },
+    },
+    lg: {
+      maxW: 'container.lg',
+      mx: 'auto',
+      paddingX: '1rem',
+      paddingY: {
+        base: 0,
+        md: '1.5rem',
+      },
+    },
+    sm: {
+      maxW: 'container.sm',
+      mx: 'auto',
+      paddingX: '1rem',
+      paddingY: {
+        base: '2.5rem',
+        md: '4rem',
+      },
+    },
+    xs: {
+      maxW: 'container.sm',
+      mx: 'auto',
+      paddingX: {
+        base: '1rem',
+        md: '6rem',
+      },
+      paddingY: {
+        base: '2.5rem',
+        md: '4rem',
+      },
+    },
+  },
+  defaultProps: {
+    variant: 'default',
+  },
+}
